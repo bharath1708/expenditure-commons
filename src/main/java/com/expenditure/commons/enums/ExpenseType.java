dbf2cbb3dc4d8fae3a4730656c98190748463838
bharath1708 <Bharath17>
2019-10-21 17:15:38 +0530

    First Commit

---
+++ src/main/java/com/expenditure/commons/enums/ExpenseType.java
@@ -0,0 +1,7 @@
+package com.expenditure.commons.enums;
+
+public enum ExpenseType {
+
+	Food, Transport, Medical, Clothes, Cosmetics, Fee, Dues
+
+}
---
+++ src/main/java/com/expenditure/commons/urlconstants/ExpenditureURLs.java
@@ -0,0 +1,7 @@
+package com.expenditure.commons.urlconstants;
+
+public class ExpenditureURLs {
+
+	public static final String ADD_EXPENSE = "/addExpense";
+		
+}
